4cd52ff755e24ca0de55a4715f08c794739124ea
ZAS0 <zeeshan.ahmed.siddiqui07@gmail.com>
2025-08-07 21:40:59 +0530

    Added Jwt Authentication

--- src/main/java/com/zeeecom/journalEntry/Config/SpringSecurity.java
+++ src/main/java/com/zeeecom/journalEntry/Config/SpringSecurity.java
@@ -1,10 +1,12 @@
 package com.zeeecom.journalEntry.Config;
 
 
+import com.zeeecom.journalEntry.Filter.JwtFilter;
 import com.zeeecom.journalEntry.Services.UserDetailsServiceIMP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,23 +14,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
-    @Autowired
     private UserDetailsServiceIMP userDetailsServiceIMP;
+    private JwtFilter jwtFilter;
+
+    public SpringSecurity(UserDetailsServiceIMP userDetailsServiceIMP,JwtFilter jwtFilter){
+        this.userDetailsServiceIMP=userDetailsServiceIMP;
+        this.jwtFilter=jwtFilter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/journal/**","/users/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .httpBasic();
+                .anyRequest().permitAll();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -40,4 +47,11 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
 }
--- src/main/java/com/zeeecom/journalEntry/Controller/PublicController.java
+++ src/main/java/com/zeeecom/journalEntry/Controller/PublicController.java
@@ -1,24 +1,60 @@
 package com.zeeecom.journalEntry.Controller;
 
+import com.zeeecom.journalEntry.Services.UserDetailsServiceIMP;
 import com.zeeecom.journalEntry.Services.UserServices;
 import com.zeeecom.journalEntry.entity.Users;
+import com.zeeecom.journalEntry.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
 
-    @Autowired
     private UserServices userServices;
+    private AuthenticationManager authenticationManager;
+    private UserDetailsServiceIMP userDetailsServiceIMP;
+    private JwtUtil jwtUtil;
+
+    public PublicController(UserServices userServices,AuthenticationManager authenticationManager,UserDetailsServiceIMP userDetailsServiceIMP,JwtUtil jwtUtil){
+        this.userServices=userServices;
+        this.authenticationManager=authenticationManager;
+        this.userDetailsServiceIMP=userDetailsServiceIMP;
+        this.jwtUtil=jwtUtil;
+    }
 
     @GetMapping("/health-check")
     public String health_check(){
         return "OK";
     }
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody Users user ) {
+    @PostMapping("/signup")
+    public void signup(@RequestBody Users user ) {
         userServices.SaveNewUser(user);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Users user ) {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                        user.getUserName(),
+                            user.getPassword()
+            ));
+            UserDetails userDetails = userDetailsServiceIMP.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occurred while createAuthenticationToken ", e);
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
